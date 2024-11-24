499ea3331d2eb88060c23cf967711fa25b92604c
Jason Waseq <103277116+jasonwaseq@users.noreply.github.com>
2024-11-24 14:30:23 -0800

    Update LinearRegression.h

--- include/LinearRegression.h
+++ include/LinearRegression.h
@@ -8,6 +8,8 @@ public:
     LinearRegression();
     void fit(const std::vector<double>& x, const std::vector<double>& y);
     double predict(double x);
+    void printModel();  // To show the model parameters
+
 private:
     double m_slope;
     double m_intercept;
